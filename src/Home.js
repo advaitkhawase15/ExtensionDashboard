6bba89f83d6212f7fa293f3426036790966b01ae
Yash Vishnoi <yashvishnoi007@gmail.com>
2023-03-09 15:17:40 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Weclome to dashboard</h1>
+        <button>Logout</button>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,69 @@
+import firebase from 'firebase/app' 
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import { getFirestore } from 'firebase/firestore/lite';
+import {sendPasswordResetEmail, getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from "firebase/auth";
+import { query, collection, getDocs, where, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSiuCZYBaIarReUHWuigHwWtXyvead8JA",
+  authDomain: "chromeextension-2adfb.firebaseapp.com",
+  databaseURL: "https://chromeextension-2adfb-default-rtdb.firebaseio.com",
+  projectId: "chromeextension-2adfb",
+  storageBucket: "chromeextension-2adfb.appspot.com",
+  messagingSenderId: "500003993423",
+  appId: "1:500003993423:web:00524dd1d09ebcef7d773a"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+
+
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+      const user = res.user;
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name,
+        authProvider: "local",
+        email,
+      });
+    
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+};
+
+
+const sendPasswordReset = async (email) => {
+    try {
+      await sendPasswordResetEmail(auth, email);
+      alert("Password reset link sent!");
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+};
+
+const logout = () => {
+    signOut(auth);
+};
+
+export {auth,db,logInWithEmailAndPassword,signInWithEmailAndPassword,registerWithEmailAndPassword,sendPasswordReset,sendPasswordResetEmail,logout};
